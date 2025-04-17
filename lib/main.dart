80738e994cf24bbe6a2bf986dd8ce8d30c825a5c
AhmedElesawy <elesawy928@gmail.com>
2025-04-17 19:37:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(FirebaseApp());
+}
+
+class FirebaseApp extends StatelessWidget {
+  const FirebaseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: InkWell(
+            child: Text(
+              'Firebase',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
